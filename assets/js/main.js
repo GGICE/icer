1e2ef396d31be3f1153292d249e144c343a7d758
GGICE <i@ice.gs>
2016-01-09 23:28:58 +0800

    Fix //

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,9 @@
     navList = document.querySelectorAll('.nav > ul > li > a');
 
   for (var i = 0; i < navList.length; i++) {
-    navList[i].className.replace(' nav-select','');
-    if(url === navList[i].href){
-      navList[i].className = navList[i].className + ' nav-select';
+    navList[i].className.replace(' select','');
+    if(url.match(/\/\/\S*/)[0] === navList[i].href.match(/\/\/\S*/)[0]){
+      navList[i].className = navList[i].className + ' select';
     }
   } 
 }());
